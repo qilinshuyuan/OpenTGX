007c6d087cae687926840214703dd10b8241a80b
Mr. Kylin <boyuegame@gmail.com>
2023-09-16 23:06:49 +0800

    update tgx core

--- tgx-core-cocos/assets/core_tgx/easy_ui_framework/waiting/UIWaiting.ts
+++ tgx-core-cocos/assets/core_tgx/easy_ui_framework/waiting/UIWaiting.ts
@@ -3,46 +3,47 @@ import { UIController } from "../UIController";
 import { UIMgr } from "../UIMgr";
 import { Layout_UIWaiting } from "./Layout_UIWaiting";
 
-const loadingTxtArr = ['Loading.','Loading..','Loading...'];
+const loadingTxtArr = ['.', '..', '...'];
 
-let _inst = null;
+let _inst: UIWaiting = null;
 
-export class UIWaiting extends UIController{
+export class UIWaiting extends UIController {
 
+    private _contentStr: string = 'Loading';
     protected onCreated(): void {
-        
+
     }
 
-    public static show():UIWaiting{
-        if(_inst){
-            return _inst;
+    public static show(contentStr?: string): UIWaiting {
+        if (!_inst) {
+            _inst = UIMgr.inst.showUI(UIWaiting);
         }
-        _inst = UIMgr.inst.showUI((UIWaiting));
+        _inst._contentStr = contentStr;
         return _inst;
     }
 
-    public static hide():void{
-        if(_inst){
+    public static hide(): void {
+        if (_inst) {
             _inst.hide();
             _inst = null;
         }
     }
 
-    protected onUpdate() { 
+    protected onUpdate() {
         let layout = this.layout as Layout_UIWaiting;
-        if(layout.loadingIcon){
+        if (layout.loadingIcon) {
             let euler = layout.loadingIcon.eulerAngles;
             let rot = (Date.now() / 1000) * 90;
-            layout.loadingIcon.setRotationFromEuler(euler.x,euler.y,rot);
+            layout.loadingIcon.setRotationFromEuler(euler.x, euler.y, rot);
         }
 
-        if(layout.loadingTxt){
+        if (layout.loadingTxt) {
             let idx = Math.floor(Date.now() / 500) % 3;
-            layout.loadingTxt.string = loadingTxtArr[idx];
+            layout.loadingTxt.string = this._contentStr + loadingTxtArr[idx];
         }
     }
 
-    onDispose(){
+    onDispose() {
         _inst = null;
     }
 }
